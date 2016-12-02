dc8a1bfcf874744e341d28374cf39eba7b8bf68c
Rebecca Ansems <ansems_rebecca@hotmail.com>
2016-12-01 22:33:58 -0400

    Destroy bricks on gem touch

---
+++ Assets/Code/Bricks.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bricks : MonoBehaviour
+{
+
+    int counter = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
