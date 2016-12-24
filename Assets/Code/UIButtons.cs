42cdb87a60ca1185687ac6d354ecde90ff806b6c
Rebecca Ansems <ansems_rebecca@hotmail.com>
2016-12-24 12:10:32 -0400

    Added options UI with additional elements
    Now: Reset scores
    Future: Switch balls

--- Assets/Code/UIButtons.cs
+++ Assets/Code/UIButtons.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class UIButtons : MonoBehaviour
 {
-    public Canvas pauseCanvas;
+    public Canvas pauseCanvas, optionsCanvas;
+    public Text highscoreOverlay;
     public Text highscore1, highscore2, highscore3, highscore4, highscore5;
+
+    public GameObject deathBar;
 
     private void Start()
     {
@@ -16,6 +20,7 @@ public class UIButtons : MonoBehaviour
             pauseCanvas.enabled = false;
             UpdateHighScores();
         }
+        optionsCanvas.enabled = false;
     }
 
     private void OnApplicationPause(bool pause)
@@ -44,21 +49,65 @@ public class UIButtons : MonoBehaviour
 
     public void ReturnButtonPressed()
     {
-        pauseCanvas.enabled = false;
+        if (pauseCanvas != null)
+        {
+            pauseCanvas.enabled = false;
+        }
+        optionsCanvas.enabled = false;
         Time.timeScale = 1;
     }
 
     void UpdateHighScores()
     {
-        highscore1.text = PlayerPrefs.GetString("PlayerDate1")+ " - " + PlayerPrefs.GetInt("PlayerScore1").ToString("0000");
-        highscore2.text = PlayerPrefs.GetString("PlayerDate2")+ " - " + PlayerPrefs.GetInt("PlayerScore2").ToString("0000");
-        highscore3.text = PlayerPrefs.GetString("PlayerDate3")+ " - " + PlayerPrefs.GetInt("PlayerScore3").ToString("0000");
-        highscore4.text = PlayerPrefs.GetString("PlayerDate4")+ " - " + PlayerPrefs.GetInt("PlayerScore4").ToString("0000");
-        highscore5.text = PlayerPrefs.GetString("PlayerDate5")+ " - " + PlayerPrefs.GetInt("PlayerScore5").ToString("0000");
+        if (highscore1 != null)
+        {
+            highscore1.text = PlayerPrefs.GetString("PlayerDate1") + " - " + PlayerPrefs.GetInt("PlayerScore1").ToString("00000000");
+            highscore2.text = PlayerPrefs.GetString("PlayerDate2") + " - " + PlayerPrefs.GetInt("PlayerScore2").ToString("00000000");
+            highscore3.text = PlayerPrefs.GetString("PlayerDate3") + " - " + PlayerPrefs.GetInt("PlayerScore3").ToString("00000000");
+            highscore4.text = PlayerPrefs.GetString("PlayerDate4") + " - " + PlayerPrefs.GetInt("PlayerScore4").ToString("00000000");
+            highscore5.text = PlayerPrefs.GetString("PlayerDate5") + " - " + PlayerPrefs.GetInt("PlayerScore5").ToString("00000000");
+
+            highscoreOverlay.text = "HIGH SCORE: " + PlayerPrefs.GetInt("PlayerScore1").ToString("00000000");
+        }
     }
 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void OnOptionsButtonPressed()
+    {
+        if (pauseCanvas != null)
+        {
+            pauseCanvas.enabled = false;
+        }
+        optionsCanvas.enabled = true;
+        Time.timeScale = 0;
+    }
+
+    public void ResetScoreButtonPressed()
+    {
+        PlayerPrefs.SetInt("PlayerScore1", 0);
+        PlayerPrefs.SetInt("PlayerScore2", 0);
+        PlayerPrefs.SetInt("PlayerScore3", 0);
+        PlayerPrefs.SetInt("PlayerScore4", 0);
+        PlayerPrefs.SetInt("PlayerScore5", 0);
+
+        PlayerPrefs.SetString("PlayerDate1", DateTime.Today.ToShortDateString());
+        PlayerPrefs.SetString("PlayerDate2", DateTime.Today.ToShortDateString());
+        PlayerPrefs.SetString("PlayerDate3", DateTime.Today.ToShortDateString());
+        PlayerPrefs.SetString("PlayerDate4", DateTime.Today.ToShortDateString());
+        PlayerPrefs.SetString("PlayerDate5", DateTime.Today.ToShortDateString());
+
+        PlayerPrefs.SetFloat("PlayerDeathLevel", 5);
+
+        if(deathBar != null)
+        {
+            deathBar.transform.position = new Vector2(0, PlayerPrefs.GetFloat("PlayerDeathLevel"));
+        }
+
+        UpdateHighScores();
+        ReturnButtonPressed();
     }
 }
